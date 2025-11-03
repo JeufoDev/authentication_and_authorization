562ec01111a23d83f9d1096708c1a07cbdaf0030
JeufoDev <llucasjefferson76@gmail.com>
2025-11-03 10:45:33 -0300

    feat: created the login endpoint, using jwt to generate the authorization token

--- src/data/dataBase.ts
+++ src/data/dataBase.ts
@@ -1,7 +1,8 @@
 export type User = {
+    id: string,
     user: string,
     password: string
 }
 
-export const users: User[] = [{user: "Jefferson", password: "testando"}];
+export const users: User[] = [];
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 import bcrypt from 'bcryptjs';
+import { randomUUID } from 'crypto'
 import 'dotenv/config'
 import { users } from './data/dataBase'
 import type { User } from './data/dataBase'
+import jwt from 'jsonwebtoken'
 
 const server = express();
 server.use(helmet());
@@ -14,7 +16,7 @@ server.post('/register', async (request: Request, response: Response) => {
     const existingUser = users.find(u => u.user === user);
 
     try {
-        if (!user || !password || user.trim() === "" || password.trim() === "") {
+        if (!user?.trim() || !password?.trim()) {
             return response.status(400).json({ "message": "Missing username or password" })
         } else if (existingUser) {
             return response.status(409).json({ "message": "Username is already in use" })
@@ -23,6 +25,7 @@ server.post('/register', async (request: Request, response: Response) => {
         const passHash = await bcrypt.hash(password, 10);
 
         const newUser: User = {
+            id: randomUUID(),
             user,
             password: passHash
         }
@@ -31,6 +34,43 @@ server.post('/register', async (request: Request, response: Response) => {
         response.status(201).json({ "message": "Ceated" })
 
     } catch (error) {
+        response.status(500).json({ error: "Internal server error." });
+    }
+});
+
+server.post('/login', async (request: Request, response: Response) => {
+    const { user, password } = request.body;
+
+    try {
+        if (!user?.trim() || !password?.trim()) {
+            return response.status(400).json({ "message": "Invalid credentials" })
+        }
+
+        const existingUser = users.find(u => u.user === user);
+        if (!existingUser) return response.status(401).json({ message: "Invalid credentials" });
+
+        const isPassword = await bcrypt.compare(password, existingUser.password);
+        if (!isPassword) return response.status(401).json({ message: "Invalid credentials" });
+
+        const payload = {
+            id: existingUser.id,
+            user: existingUser.user
+        }
+
+        if (!process.env.JWT_KEY) {
+            throw new Error("JWT_KEY não definida");
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1h' })
+
+        response.status(200).json({
+            message: "Login successful",
+            user: payload,
+            token,
+            expiresIn: '1h'
+        })
+    } catch (error) {
+        console.error(error);
         response.status(500).json({ error: "Internal server error." });
     }
 });
